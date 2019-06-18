1f98c6fbb5e03081116d43d209a5fbd716ffca40
Dima <dimon.s186@yandex.ru>
2019-06-18 13:22:13 +0700

    begin layout

---
+++ resources/views/header_footer/header.blade.php
@@ -0,0 +1,13 @@
+<header>
+	<nav>
+		<ul class="header-top">
+			<li><a href="#">Главная</a></li>
+			<li><a href="#">Конкурсы</a></li>
+			<li><a href="#">Архив конкурсов</a></li>
+			<li><a href="#">Публикации</a></li>
+		</ul>
+	</nav>
+	<div class="header-bottom">
+		<img src="{{asset('images/logo.png')}}" alt="">
+	</div>
+</header>
---
+++ resources/views/main.blade.php
@@ -0,0 +1,11 @@
+<!doctype html>
+<html lang="ru">
+<head>
+	<title>Главная страница</title>
+	@include('styles')
+</head>
+<body>
+	@include('header_footer.header')
+	@include('script')
+</body>
+</html>
