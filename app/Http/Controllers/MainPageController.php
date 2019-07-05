764dd501b38f18e16301b80ca45e82df6c3541af
eroha <dimon.s186@yandex.ru>
2019-07-05 12:02:37 +0700

    migration add foreign

--- app/Http/Controllers/MainPageController.php
+++ app/Http/Controllers/MainPageController.php
@@ -10,8 +10,13 @@ class MainPageController extends Controller
 {
 	public function show()
 	{
+<<<<<<< HEAD
     $data = Publication::find(1);
     dump($data);
 		//return view('main', ['data' => $data]);
+=======
+    $data = Publication::all()->get();
+		return view('main', ['data' => $data]);
+>>>>>>> 659d1a7... migration
 	}
 }
