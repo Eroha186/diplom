127360744b960aa13bf60b904773e4e863d0f524
eroha <dimon.s186@yandex.ru>
2019-07-18 12:34:59 +0700

    ошибки валидации + перевод ошибок  валидации

--- app/Http/Requests/FormPublicationRequest.php
+++ app/Http/Requests/FormPublicationRequest.php
@@ -3,6 +3,7 @@
 namespace App\Http\Requests;
 
 use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Support\Facades\Auth;
 
 class FormPublicationRequest extends FormRequest
 {
@@ -13,7 +14,7 @@ class FormPublicationRequest extends FormRequest
    */
   public function authorize()
   {
-    return false;
+    return true;
   }
 
   /**
@@ -23,18 +24,66 @@ class FormPublicationRequest extends FormRequest
    */
   public function rules()
   {
-    return [
+    if(Auth::check()) {
+      return [
+
+        'kind' => 'required|min:0',
+        'title' => 'required|string',
+        'type' => 'required|min:0',
+        'annatation' => 'required|string|max:100',
+        'files' => 'required',
         'files.*' => ['file', 'required|mimes:jpg,png,doc,docx,pdf,ppt,pptx'],
-    ];
+        'offer' => 'accepted',
+        'processing-pd' => 'accepted',
+      ];
+    } else {
+      return [
+        'f' => 'required|string|max:30',
+        'i' => 'required|string|max:30',
+        'o' => 'required|string|max:30',
+        'job' => 'required|string',
+        'stuff' => 'required|string',
+        'email' => 'required|email|max:60|unique:users',
+        'town' => 'required|string',
+        'education' => 'required|min:0',
+        'kind' => 'required|min:0',
+        'title' => 'required|string',
+        'type' => 'required|min:0',
+        'annatation' => 'required|string|max:100',
+        'files' => 'required',
+        'files.*' => ['file', 'required|mimes:jpg,png,doc,docx,pdf,ppt,pptx'],
+        'offer' => 'accepted',
+        'processing-pd' => 'accepted',
+      ];
+    }
   }
 
   public function messages()
   {
 
     return [
+        'f.required' => 'Укажите вашу Фамилию',
+        'i.required' => 'Укажите ваше Имя',
+        'o.required' => 'Укажите ваше Отчество',
+        'job.required' => 'Укажите вашу Должность',
+        'email' => [
+          'required' => 'Укажите ваш E-mail',
+          'email' => 'Некорректный адрес электронной почты',
+          'unique' => 'Пользователь с таким E-mail существует',
+        ],
+        'stuff.required' => 'Укажите наименования вашего образовательного учреждения',
+        'town.required' => 'Укажите ваш город',
+        'education.required' => 'Укажите уровень образования',
+        'kind.required' => 'Укажите вид публикации',
+        'title.required' => 'Укажите название работы',
+        'type.required' => 'Укажите тип работы',
+        'themes.required' => 'Укажите тематику работы',
+        'annatation.required' => 'Укажите описание работы',
         'files.required' => 'Не забудьте приложить файлы',
         'files.*.file' => 'Должен быть файл',
         'files.*.mime' => 'Не поддерживаемый формат',
+        'offer.accepted' => 'Согласие с условиями оферты должно быть принято',
+        'processing-pd' => 'Согласие на обработку персональных данных должно быть принято',
     ];
   }
 }
