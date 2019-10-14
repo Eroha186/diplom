be1fee496cd1d1c5b0a6d51a955022aae6d823da
eroha <dimon.s186@yandex.ru>
2019-10-14 20:47:10 +0700

    Добавлена библиотека для работы с изображениями

---
+++ app/Http/Controllers/Reward/GenerationDiplom.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Http\Controllers\Reward;
+
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+
+class GenerationDiplom extends Controller
+{
+
+}
--- config/app.php
+++ config/app.php
@@ -176,7 +176,7 @@ return [
         // App\Providers\BroadcastServiceProvider::class,
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
-
+        Intervention\Image\ImageServiceProvider::class,
         DaveJamesMiller\Breadcrumbs\ServiceProvider::class,
     ],
 
@@ -192,7 +192,7 @@ return [
     */
 
     'aliases' => [
-
+        'Image' => Intervention\Image\Facades\Image::class,
         'App' => Illuminate\Support\Facades\App::class,
         'Artisan' => Illuminate\Support\Facades\Artisan::class,
         'Auth' => Illuminate\Support\Facades\Auth::class,
