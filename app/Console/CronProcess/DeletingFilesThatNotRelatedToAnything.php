a5e4760bdce5493e74caf5443e9f56bc4e2f9c96
eroha <dimon.s186@yandex.ru>
2020-04-17 14:02:03 +0700

    Правки в процессе по удалению файлов

--- app/Console/CronProcess/DeletingFilesThatNotRelatedToAnything.php
+++ app/Console/CronProcess/DeletingFilesThatNotRelatedToAnything.php
@@ -6,15 +6,21 @@ namespace App\Console\CronProcess;
 
 use App\File;
 use Carbon\Carbon;
+use Illuminate\Support\Facades\Storage;
 
 class DeletingFilesThatNotRelatedToAnything
 {
     static public function deleteFiles()
     {
-        File::where([
+        $files = File::where([
             ['work_id', 0],
-            ['publication_id', 0],
+            ['publ_id', 0],
             ['updated_at', '<=', Carbon::now()->subHour(12)]
-        ])->delete();
+        ])->get();
+        $disk = Storage::disk('public');
+        foreach($files as $file) {
+            $disk->delete($file->url);
+            File::where('id', $file->id)->delete();
+        }
     }
 }
