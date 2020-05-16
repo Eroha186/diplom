65502defd376228dce75728c0d42ddf7ecaf5ce9
eroha <dimon.s186@yandex.ru>
2020-05-16 10:49:23 +0700

    Правка в шаблоне авторизации через соц сети

--- resources/views/auth/social.blade.php
+++ resources/views/auth/social.blade.php
@@ -2,10 +2,10 @@
     <a class="btn btn-link" href="{{route('auth.social', 'vkontakte')}}">
         <img style="height: 32px; width: 32px;" src="{{asset('images/vk-social.svg')}}" alt="">
     </a>
-    <a class="btn btn-link" href="{{route('auth.social', 'vkontakte')}}">
+    <a class="btn btn-link" href="{{route('auth.social', 'odnoklassniki')}}">
         <img src="{{asset('images/ok-social.svg')}}" alt="">
     </a>
-    <a class="btn btn-link"  href="{{route('auth.social', 'vkontakte')}}">
+    <a class="btn btn-link"  href="{{route('auth.social', 'yandex')}}">
         <img  style="height: 32px; width: 32px;" src="{{asset('images/yandex-social.svg')}}" alt="">
     </a>
 </div>
