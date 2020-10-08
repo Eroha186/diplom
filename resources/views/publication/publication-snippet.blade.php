ab980b4fab9e52bd139e1f598ff0dffd08339f06
chocoway <chocovvay@gmail.com>
2020-10-08 17:04:00 +0700

    Верстка публикаций

--- resources/views/publication/publication-snippet.blade.php
+++ resources/views/publication/publication-snippet.blade.php
@@ -1,6 +1,26 @@
+
 <div class="row p15">
     @foreach($publications as $publication)
-        <div class="col-xl-12 publication-card">
+
+   
+<div class="col-12 d-flex main-page__last_pub-item">
+                <img src="{{asset('images/main_page/doc.png')}}" style="width: 32px; height: 40px; margin-right: 16px;" alt="">
+                <div>
+                   <a href="/publication/{{$publication->id}}">{{$publication->title}}</a>
+                  <p><span>{{$publication->kind->name}} • {{$publication->education->name}}</span>            <span>{{$publication->type->name}} • Весна • Осень</span></p>
+                    <p> {{$publication->annotation}}</p>
+                    <div class="d-flex mb-2">
+                        <a href="{{asset('images/main_page/cat1.png')}}" data-fancybox="gallery"><img src="{{asset('images/main_page/cat1.png')}}" alt=""></a>
+                         <a href="{{asset('images/main_page/cat2.png')}}" data-fancybox="gallery"><img src="{{asset('images/main_page/cat2.png')}}" alt=""></a>
+                         <a href="{{asset('images/main_page/cat3.png')}}" data-fancybox="gallery"><img src="{{asset('images/main_page/cat3.png')}}" alt=""></a>
+                    </div>
+                    <p class="main-page__last_pub-item-3 mb-0"><span>{{ date("d.m.Y", strtotime($publication->date_add)) }}</span>    <span>{{$publication->user->f}} {{$publication->user->i}} {{$publication->user->o}},
+                        {{$publication->user->stuff}},
+                        {{$publication->user->town}},
+                        {{$publication->user->job}}</span></p>
+                </div>
+            </div>
+       <!--  <div class="col-xl-12 publication-card">
             <div class="row">
                 <div class="col-xl-10">
                     <div class="publication-card__title">
@@ -43,7 +63,7 @@
                     <div class="tag">{{$theme->name}}</div>
                 @endforeach
             </div>
-        </div>
+        </div> -->
 
     @endforeach
 </div>
