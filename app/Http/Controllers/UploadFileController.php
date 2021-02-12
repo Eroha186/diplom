b363f791e7d00d4bb823c2541ead56bb68003d43
chocoway <chocovvay@gmail.com>
2021-02-12 16:18:08 +0700

    Добавление thumbnail

--- app/Http/Controllers/UploadFileController.php
+++ app/Http/Controllers/UploadFileController.php
@@ -10,63 +10,76 @@ use Illuminate\Support\Facades\Validator;
 
 class UploadFileController extends Controller
 {
-    public function uploadFile($file, $typeFile, $work_id = 0)
-    {
-        $type = $file->getMimeType();
-        switch ($type) {
-            case ('application/pdf'):
-            $type = 'pdf';
-            break;
-            case ('image/jpeg'):
-            case ('image/png'):
-            case ('image/jpg'):
-            $type = 'image';
-            break;
-            case ('application/vnd.openxmlformats-officedocument.wordprocessingml.document'):
-            case ('application/msword'):
-            $type = 'doc';
-            break;
-            case ('application/vnd.openxmlformats-officedocument.presentationml.presentation'):
-            case ('application/vnd.ms-powerpoint'):
-            $type = 'ppt';
-            break;
-        }
-        $path = $file->store('upload', 'public');
-        return File::create([
-            'publ_id' => $typeFile === 'publication' ? $work_id : 0,
-            'url' => $path,
-            'type' => $type,
-            'work_id' => $typeFile === 'competition' ? $work_id : 0,
-        ]);
-    }
+	public function uploadFile($file, $typeFile, $work_id = 0)
+	{
+		$type = $file->getMimeType();
+		switch ($type) {
+			case ('application/pdf'):
+			$type = 'pdf';
+			break;
+			case ('image/jpeg'):
+			case ('image/png'):
+			case ('image/jpg'):
+			$type = 'image';
+			break;
+			case ('application/vnd.openxmlformats-officedocument.wordprocessingml.document'):
+			case ('application/msword'):
+			$type = 'doc';
+			break;
+			case ('application/vnd.openxmlformats-officedocument.presentationml.presentation'):
+			case ('application/vnd.ms-powerpoint'):
+			$type = 'ppt';
+			break;
+		}
+		$path = $file->store('upload', 'public');
+		return File::create([
+			'publ_id' => $typeFile === 'publication' ? $work_id : 0,
+			'url' => $path,
+			'type' => $type,
+			'work_id' => $typeFile === 'competition' ? $work_id : 0,
+		]);
+	}
 
-    public function uploaderPublication(Request $request)
-    {
-        $validator = Validator::make($request->all(), [
-            'file' => 'required|mimes:jpg,jpeg,png,doc,docx,pdf,ppt,pptx'
-        ]);
+	public function uploaderPublication(Request $request)
+	{
+		$validator = Validator::make($request->all(), [
+			'file' => 'required|mimes:jpg,jpeg,png,doc,docx,pdf,ppt,pptx'
+		]);
 
-        if($validator->passes())
-        {
+		if($validator->passes())
+		{
 
 
-            $fileUpload = $this->uploadFile($request->file('file'), 'publication');
-            $new_path = storage_path().'/app/public/'.$fileUpload->url;
-            $resize = Image::make($new_path)->resize(env('IMAGE_WIDTH'), env('IMAGE_HEIGHT'), function ($constraint) {
-                $constraint->aspectRatio();
-                $constraint->upsize();
-            })->save($new_path);
-           
-            return $fileUpload->id;
-        }
-        return response()->json(['errors' => $validator->errors()->all()]);
-    }
+			$fileUpload = $this->uploadFile($request->file('file'), 'publication');
+			$new_path = storage_path().'/app/public/'.$fileUpload->url;
+			$type = explode('.', $new_path);
+			if($type[1] == 'png') {
+				$filePath = $type[0].'.jpg';
+			}else {
+				$filePath = $new_path;
+			}
+			$resize = Image::make($new_path)->resize(env('PUBL_IMAGE_WIDTH'), env('PUBL_IMAGE_HEIGHT'), function ($constraint) {
+				$constraint->aspectRatio();
+				$constraint->upsize();
+			})->encode('jpg', env('PUBL_QUALITY'))->save($filePath);
+			$thumbPath = str_replace('upload/', 'upload/thumb/', $filePath);
+			// return $filePath;
+			$resize = Image::make($filePath)->resize(env('THUMB_PUBL_IMAGE_WIDTH'), env('THUMB_PUBL_IMAGE_HEIGHT'), function ($constraint) {
+				$constraint->aspectRatio();
+				$constraint->upsize();
+			})->encode('jpg', env('THUMB_PUBL_QUALITY'))->save($thumbPath);
+			return $fileUpload->id;
 
-    
-    public function attachPublicationIdForFile($file, $publicationId)
-    {
-        $fileModel = File::where('id', '=', $file)->first();
-        $fileModel->publ_id = $publicationId;
-        $fileModel->save();
-    }
+
+		}
+		return response()->json(['errors' => $validator->errors()->all()]);
+	}
+
+
+	public function attachPublicationIdForFile($file, $publicationId)
+	{
+		$fileModel = File::where('id', '=', $file)->first();
+		$fileModel->publ_id = $publicationId;
+		$fileModel->save();
+	}
 }
